beee3e1d1bed8d14cab871604daabeffcefe53af
verhelstbert@gmail.com <verhelstbert@gmail.com@faef55cb-fcfc-8f59-b399-a3efa32c2002>
2012-04-17 20:52:01 +0000

    added analysis window (no functionality yet) optimized actor and movie window and poster slider control

---
+++ trunk/moviemanager/MovieManager.APP/Panels/Analyse/AnalyseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using Model;
+using SQLite;
+
+namespace MovieManager.APP.Panels.Analyse
+{
+    class AnalyseController
+    {
+        public AnalyseController()
+        {
+            Videos.Clear();
+            MMDatabase.SelectAllVideos(Videos);
+        }
+
+        private ObservableCollection<Video> _videos = new ObservableCollection<Video>();
+        public ObservableCollection<Video> Videos
+        {
+            get
+            {
+                return _videos;
+            }
+            set
+            {
+                _videos = value;
+            }
+        }
+    }
+}
---
+++ trunk/moviemanager/MovieManager.APP/Panels/Analyse/AnalyseWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieManager.APP.Panels.Analyse
+{
+    /// <summary>
+    /// Interaction logic for AnalysePanel.xaml
+    /// </summary>
+    public partial class AnalyseWindow
+    {
+        private AnalyseController _controller;
+
+        public AnalyseWindow()
+        {
+            InitializeComponent();
+            _controller = new AnalyseController();
+            DataContext = _controller;
+        }
+    }
+}
